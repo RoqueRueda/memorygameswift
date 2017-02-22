96d6427bc34cefae0df9079296bf38b9ef9e9907
Roque Rueda <skaroque@gmail.com>
2017-02-22 00:57:05 -0600

    # Adding collection view datasource

--- MemoryGame/ViewController.swift
+++ MemoryGame/ViewController.swift
@@ -12,10 +12,14 @@ class ViewController: UIViewController {
 
     @IBOutlet weak var memoryBoard: UICollectionView!
     
+    var memoryGame: MemoryGame = MemoryGame();
     
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        memoryGame.card = memoryGame.startGame(cardImages: memoryGame.cardImages)
+        memoryBoard.dataSource = self
     }
 
     override func didReceiveMemoryWarning() {
@@ -24,5 +28,28 @@ class ViewController: UIViewController {
     }
 
 
+}
+
+extension ViewController : UICollectionViewDataSource {
+    
+    func numberOfSections(in collectionView: UICollectionView) -> Int {
+        return 1
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return memoryGame.card.count
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCell(
+            withReuseIdentifier: "cardCell", for: indexPath) as! CardCellCollectionViewCell
+        
+        cell.id = memoryGame.card[indexPath.row].id
+        cell.image.image = memoryGame.card[indexPath.row].image
+        
+        return cell;
+        
+    }
+    
 }
 
