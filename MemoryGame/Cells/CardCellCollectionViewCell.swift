0adf6176d19af24cd7093df94f5e28c26bac8a4e
Roque Rueda <skaroque@gmail.com>
2017-02-22 00:28:33 -0600

    # Creating outlets between UIViews and custom classes

---
+++ MemoryGame/Cells/CardCellCollectionViewCell.swift
@@ -0,0 +1,18 @@
+//
+//  CardCellCollectionViewCell.swift
+//  MemoryGame
+//
+//  Created by Roque Rueda on 22/02/17.
+//  Copyright © 2017 HCL. All rights reserved.
+//
+
+import UIKit
+
+class CardCellCollectionViewCell: UICollectionViewCell {
+
+    var img     : UIImage = UIImage()
+    var id      : Int = 0
+    
+    @IBOutlet weak var image: UIImageView!
+    
+}
